68bba2f0128c54f2434057529016d7cb5f88102c
Alejo <istominalexey@ro.ru>
2024-07-17 21:08:51 +0300

    Task2 realised

--- Files_working.py
+++ Files_working.py
@@ -12,4 +12,24 @@ with open ('recipes.txt', encoding = 'utf-8') as f:
             ing_dish.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
             cook_book[dish] = ing_dish
         f.readline()
-    print(f' Cook_book= {cook_book}')
+    pprint.pprint(f' Cook_book= {cook_book}')
+
+print('')
+    
+    
+
+def get_shop_list_by_dishes(dishes, person_count):
+    ings_shop_list = {}
+    for dish in dishes:
+        for ing_dish in cook_book[dish]:
+            if dish in cook_book:
+                prod = ing_dish['ingredient_name']
+                ings_shop_list.setdefault(prod, [])
+                ings = int(ing_dish['quantity']) * person_count
+                ing_list = []
+                ing_list.append({'quantity': ings, 'measure': ing_dish['measure']})
+                ings_shop_list[prod] = ing_list
+    pprint.pprint(ings_shop_list)
+    return ings_shop_list
+
+get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2)
